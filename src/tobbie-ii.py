abb0fd4d4fa967b3d9f0ea239adea2d86388bdd7
Matt Pavlovich <matt@hyte.io>
2020-12-28 20:42:05 -0600

    Prefix all functions with tobbieii_
     - Add missing rotate_left function

--- src/tobbie-ii.py
+++ src/tobbie-ii.py
@@ -1,29 +1,34 @@
 from microbit import *
 
 def tobbieii_walk_forward():
-    print('walk_forward() invoked')
+    print('tobbieii_walk_forward() invoked')
     if pin8.read_digital():
         pin13.write_digital(1)
         pin14.write_digital(0)
-        
+
 def tobbieii_walk_backward():
-    print('walk_backward() invoked')
+    print('tobbieii_walk_backward() invoked')
     if pin8.read_digital():
         pin13.write_digital(0)
         pin14.write_digital(1)
 
 def tobbieii_walk_stop():
-    print('walk_stop() invoked')
+    print('tobbieii_walk_stop() invoked')
     pin13.write_digital(0)
     pin14.write_digital(0)
-    
+
+def tobbieii_rotate_left():
+    print('tobbieii_rotate_left() invoked')
+    pin15.write_digital(1)
+    pin16.write_digital(0)
+   
 def tobbieii_rotate_right():
-    print('rotate_right() invoked')
+    print('tobbieii_rotate_right() invoked')
     pin15.write_digital(0)
     pin16.write_digital(1)
 
 def tobbieii_rotate_stop():
-    print('rotate_stop() invoked')
+    print('tobbieii_rotate_stop() invoked')
     pin15.write_digital(0)
     pin16.write_digital(0)
 
@@ -31,14 +36,20 @@ while True:
     display.scroll('Hello, World!')
     display.show(Image.HEART)
     sleep(2000)
-    rotate_right()
+    tobbieii_rotate_right()
     sleep(2000)
-    rotate_stop()
+    tobbieii_rotate_stop()
     sleep(2000)
-    walk_forward()
+    tobbieii_rotate_left()
     sleep(2000)
-    walk_stop()
+    tobbieii_rotate_stop()
     sleep(2000)
-    walk_backward()
+    tobbieii_walk_forward()
     sleep(2000)
-    walk_stop()
+    tobbieii_walk_stop()
+    sleep(2000)
+    tobbieii_walk_backward()
+    sleep(2000)
+    tobbieii_walk_stop()
+    sleep(2000)
+
